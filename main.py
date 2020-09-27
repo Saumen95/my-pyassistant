afa4e15ca8e9ba38672bbda3ab45617eade56227
Saumen95 <saumenroy323@gmail.com>
2020-09-28 00:56:21 +0600

    finished

--- main.py
+++ main.py
@@ -7,50 +7,79 @@ from gtts import gTTS
 import playsound
 import random
 
+class Person:
+    name = ''
+
+    def setname(self, name):
+        self.name = name
+
+def there_exits(terms):
+    for term in terms:
+        if term in voice_data:
+            return True
+
 r = sr.Recognizer()
 
 
-def record_audio():
+def record_audio(ask=False):
     with sr.Microphone as source:
+        if ask:
+            speak(ask)
         audio = r.listen(source)
         voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
 
         except sr.UnknownValueError:
-            print('Sorry, dont understand')
+            speak('Sorry, dont understand')
 
         except sr.RequestError:
-            print('Sorry, Service is Down')
+            speak('Sorry, Service is Down')
+
+        print(f">> {voice_data.lower()}")
 
         return voice_data
 
+
+def speak(audio_string):
+    tts = gTTS(text=audio_string, lang='en')
+    r = random.randint(1, 100000)
+    audio_file = 'audio' + str(r) + '.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(f"kiri: {audio_string}")
+    os.remove(audio_file)
+
+
+
 def respond(voice_data):
-    if 'What is your name' in voice_data:
-        i = input()
-        print('My name is' + i)
+    with sr.Microphone as source:
+        audio = r.listen()
+        if 'what is your name' in voice_data:
+            speak(audio)
 
-    if 'What Time is it' in voice_data:
-        print(ctime())
+        if 'what time is it' in voice_data:
+            speak(ctime())
 
-    if 'search' in voice_data:
-        search = record_audio('What are you searching for?')
-        url = 'https://www.google.com/?q=' + search
-        webbrowser.get().open(url)
-        print('here you go'+ search)
+        if 'search' in voice_data:
+            search = record_audio('what are you looking for?')
+            url = 'https://www.google.com/serch?q=' + search
+            webbrowser.get().open(url)
+            speak('here you go' + search)
 
-    if 'Find Location' in voice_data:
-        location = record_audio('Where am I?')
-        url = 'https://www.google.com/maps/@23.6013305,89.8411966,14z/places' + location
-        webbrowser.get().open(url)
-        print('You are here'+ location)
+        if 'Find location' in voice_data:
+            location = record_audio('where am I?')
+            url = 'https://www.google.com/maps/@23.6013305,89.8411966,14z' + location
+            webbrowser.get().open(url)
+            speak('Here you are' + location)
 
-    if 'exit' in voice_data:
-        exit()
+        if 'exit' in voice_data:
+            exit()
+
 
 
 time.sleep(2)
-print('How Can I Help You?')
+speak('How Can I Help You?')
 while 2:
     voice_data = record_audio()
     respond(voice_data)
